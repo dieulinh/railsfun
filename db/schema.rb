4b7c59baee6913cdbf72e2dbdd69a41006ef7b62
dieulinh <linhvn09@gmail.com>
2015-07-06 14:46:55 +0700

    add categories db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706073354) do
+ActiveRecord::Schema.define(version: 20150706073706) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
