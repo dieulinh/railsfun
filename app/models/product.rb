3800e943c024109c5fe024d1684f55f2436acdec
dieulinh <linhvn09@gmail.com>
2015-07-07 10:01:16 +0700

    use scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,15 @@
 class Product < ActiveRecord::Base
 	validate :title_is_shorter_than_description
 	validates :price, numericality: { greater_than: 0 }, presence: true
+
+	scope :published, -> { where(published: true) }
+	scope :price_more_than, -> (price) { where('price > ?', price)}
+	scope :description_includes, -> (text) { where('description LIKE ?', "%#{text}%") }
+
 	def title_is_shorter_than_description
 		return if title.blank? or description.blank?
 		if title.length >= description.length
-			errors.add(:description, "cann't be shorter than title")
+			errors.add(:description, "can't be shorter than title")
 		end
 	end
 end
