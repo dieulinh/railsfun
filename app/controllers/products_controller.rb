f0c9868944f065e39ee682a1ed0ca0d4c1a8545e
dieulinh <linhvn09@gmail.com>
2015-07-07 16:27:39 +0700

    add show action for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,7 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all.includes(:category).published
 	end
+	def show
+		@product = Product.find(params[:id])
+	end
 end
