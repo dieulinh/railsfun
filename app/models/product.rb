2b879470de165f8a260510b67d27472195a20fa7
dieulinh <linhvn09@gmail.com>
2015-07-08 13:33:12 +0700

    fix country list and specify ruby version and add rails_12factor to gem file for production

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 	extend Enumerize
 
   enumerize :difficulty_level, in: [:easy, :medium, :hard]
-  enumerize :country_of_origin, in: Country.all.map { |country_name, country_code| country_code }
+  enumerize :country_of_origin, in: Country.all
 
 	validate :title_is_shorter_than_description
 	validates :price, numericality: { greater_than: 0 }, presence: true
