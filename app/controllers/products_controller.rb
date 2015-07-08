17ae93eb7f046deee7f4464601bd171f0d2de581
dieulinh <linhvn09@gmail.com>
2015-07-08 10:05:21 +0700

    use better errors, binding_of_caller gem for development and add create method to submit the form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,32 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all.includes(:category).published
 	end
+	
 	def show
 		@product = Product.find(params[:id])
 	end
+	
 	def new
 		@product = Product.new
+	end
+
+	def create
+		@product = Product.create(product_params)
+		if @product.save
+			redirect_to products_url
+		else
+			render :new
+		end	
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit( :title,
+																		 :description,
+																		 :price,
+																		 :published,
+																		 :category_id,
+																		 :country_of_origin )
 	end
 end
